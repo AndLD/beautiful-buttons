310bc9028c2e4b6b6f1d2dbe6f72c7dcbb52930a
AndLD <nil10035@gmail.com>
2021-11-04 10:21:37 +0200

    beautiful-buttons completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,149 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { isMobile } from 'react-device-detect'
+
+import { FiBookmark } from 'react-icons/fi'
+import { FiUsers } from 'react-icons/fi'
+import { FiShoppingBag } from 'react-icons/fi'
+import { FiPieChart } from 'react-icons/fi'
+
+import './App.scss'
+import Btn from './components/Btn'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const pressedKeyState = useState(0)
+
+    return (
+        <div className="App">
+            <div style={{ top: isMobile ? 'auto' : '50px', bottom: isMobile ? '25px' : 'auto' }} className="comment">
+                {'// Анти-стресс кнопки ждут, когда вы их нажмете :)'}
+            </div>
+            <div
+                style={{ display: isMobile ? 'block' : 'flex', height: isMobile ? 'auto' : '100vh' }}
+                className="container"
+            >
+                <div>
+                    <div
+                        style={{
+                            borderRadius: isMobile ? 0 : '25px'
+                        }}
+                        className="content-wrapper"
+                    >
+                        <div className="subtitle">1. Тип сайта:</div>
+                        <div className="types">
+                            <Btn
+                                index={1}
+                                pressedKeyState={pressedKeyState}
+                                icon={
+                                    <div>
+                                        <svg width="0" height="0">
+                                            <linearGradient
+                                                gradientTransform="rotate(90)"
+                                                id="blue-gradient"
+                                                x1="100%"
+                                                y1="100%"
+                                                x2="0%"
+                                                y2="0%"
+                                            >
+                                                <stop stopColor="#12c3fa" offset="60%" />
+                                                <stop stopColor="#0461ea" offset="100%" />
+                                            </linearGradient>
+                                        </svg>
+                                        <FiUsers
+                                            style={{ stroke: pressedKeyState[0] === 1 ? 'url(#blue-gradient)' : '' }}
+                                        />
+                                    </div>
+                                }
+                                type="Сайт-визитка"
+                                time="до 5 дней"
+                                price="от 700 грн."
+                            />
+                            <Btn
+                                index={2}
+                                pressedKeyState={pressedKeyState}
+                                icon={
+                                    <div>
+                                        <svg width="0" height="0">
+                                            <linearGradient
+                                                gradientTransform="rotate(90)"
+                                                id="blue-gradient"
+                                                x1="100%"
+                                                y1="100%"
+                                                x2="0%"
+                                                y2="0%"
+                                            >
+                                                <stop stopColor="#12c3fa" offset="60%" />
+                                                <stop stopColor="#0461ea" offset="100%" />
+                                            </linearGradient>
+                                        </svg>
+                                        <FiBookmark
+                                            style={{ stroke: pressedKeyState[0] === 2 ? 'url(#blue-gradient)' : '' }}
+                                        />
+                                    </div>
+                                }
+                                type="Лендинг"
+                                time="до 5 дней"
+                                price="от 1000 грн."
+                            />
+                            <Btn
+                                index={3}
+                                pressedKeyState={pressedKeyState}
+                                icon={
+                                    <div>
+                                        <svg width="0" height="0">
+                                            <linearGradient
+                                                gradientTransform="rotate(90)"
+                                                id="blue-gradient"
+                                                x1="100%"
+                                                y1="100%"
+                                                x2="0%"
+                                                y2="0%"
+                                            >
+                                                <stop stopColor="#12c3fa" offset="60%" />
+                                                <stop stopColor="#0461ea" offset="100%" />
+                                            </linearGradient>
+                                        </svg>
+                                        <FiPieChart
+                                            style={{ stroke: pressedKeyState[0] === 3 ? 'url(#blue-gradient)' : '' }}
+                                        />
+                                    </div>
+                                }
+                                type="Сайт для бизнеса"
+                                time="от 7 дней"
+                                price="от 3000 грн."
+                            />
+                            <Btn
+                                index={4}
+                                pressedKeyState={pressedKeyState}
+                                icon={
+                                    <div>
+                                        <svg width="0" height="0">
+                                            <linearGradient
+                                                gradientTransform="rotate(90)"
+                                                id="blue-gradient"
+                                                x1="100%"
+                                                y1="100%"
+                                                x2="0%"
+                                                y2="0%"
+                                            >
+                                                <stop stopColor="#12c3fa" offset="60%" />
+                                                <stop stopColor="#0461ea" offset="100%" />
+                                            </linearGradient>
+                                        </svg>
+                                        <FiShoppingBag
+                                            style={{ stroke: pressedKeyState[0] === 4 ? 'url(#blue-gradient)' : '' }}
+                                        />
+                                    </div>
+                                }
+                                type="Интернет-магазин"
+                                time="от 7 дней"
+                                price="от 5000 грн."
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/components/Btn.tsx
@@ -0,0 +1,28 @@
+import { isMobile } from 'react-device-detect'
+
+export default function Btn({
+    index,
+    pressedKeyState,
+    icon,
+    type,
+    time,
+    price
+}: {
+    index: number
+    pressedKeyState: any
+    icon: any
+    type: string
+    time: string
+    price: string
+}) {
+    const [pressedKey, setPressedKey] = pressedKeyState
+
+    return (
+        <div onClick={() => setPressedKey(index)} className={`btn${pressedKey === index ? ' selected' : ''}`}>
+            <div className="type-icon">{icon}</div>
+            <div className={`type${pressedKey === index ? ' gradient-text' : ''}`}>{type}</div>
+            <div className="estimated-time">{time}</div>
+            <div className="estimated-price">{price}</div>
+        </div>
+    )
+}
